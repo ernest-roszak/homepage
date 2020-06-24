8ada14c4edd247e9f1300b501ec3637284a2a796
Ernest Roszak <ernest.roszak89@gmail.com>
2020-06-24 15:20:03 +0200

    webpack added

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const name = 'Ernest';
 console.log(name);
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+    T : "U "
+    
+}));
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+console.log(moment);
+console.log('Hello World');
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
